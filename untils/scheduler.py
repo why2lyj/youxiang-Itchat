ee2c69b9b46fd82d184423af85a8be9f995166e0
Snow Wang <31466760+why2lyj@users.noreply.github.com>
2020-07-08 11:23:18 +0800

    add pdd function

--- untils/scheduler.py
+++ untils/scheduler.py
@@ -4,6 +4,7 @@ from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 from untils import config
 from coupon.tb import tb_share_text
 from coupon.jd import jingfen_query
+from coupon.pdd import pdd_share_text
 
 def job_tasks():
 
@@ -11,6 +12,7 @@ def job_tasks():
 
     tb_job_tasks(scheduler)
     jd_job_task(scheduler)
+    pdd_job_task(scheduler)
 
     # 加一个监控
     scheduler.add_listener(scheduler_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
@@ -59,6 +61,30 @@ def  jd_job_task(scheduler):
                           kwargs={'group_name': chat_group['group_name'], 'material_id': chat_group['group_material_id'],
                                   'app_key': app_key, 'app_secret': app_secret, 'site_id': site_id, 'suo_im': suo_im},
                           trigger='cron', hour=f'''{chat_group['hour']}''', minute=f'''{chat_group['minute']}''', second=0,  jitter=300, id=f'''{chat_group['group_name']}''')
+
+
+def pdd_job_task(scheduler):
+
+    conf = config.get_yaml()
+    conf = conf.get('pinduoduo')
+    if not conf.get('is_open'):
+        return
+
+    if conf.get('app_key') == '' or conf.get('app_secret') == '' or conf.get('p_id') == '':
+        return
+
+    app_key = conf.get('app_key')
+    app_secret = conf.get('app_secret')
+    p_id = conf.get('p_id')
+
+    chat_groups = conf.get('chat_groups')
+    for chat_group in chat_groups:
+        print(chat_group['group_name'])
+        scheduler.add_job(func=pdd_share_text,
+                          kwargs={'group_name': chat_group['group_name'], 'group_material_id': chat_group['group_material_id'],
+                                  'app_key': app_key, 'secret_key': app_secret, 'p_id': p_id},
+                          trigger='cron', hour=f'''{chat_group['hour']}''', minute=f'''{chat_group['minute']}''', second=0,  jitter=0, id=f'''{chat_group['group_name']}''')
+
 
 def scheduler_listener(event):
     '''
